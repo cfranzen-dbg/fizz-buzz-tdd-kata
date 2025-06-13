fe39c407dffc9a773eff843630fff4afa1668ff4
Christian Franzen <christian.franzen@ecc.de>
2025-06-13 19:52:56 +0200

    Implemented simple FizzBuzz with TDD

--- fizzbuzz-simple/src/main/kotlin/FizzBuzz.kt
+++ fizzbuzz-simple/src/main/kotlin/FizzBuzz.kt
@@ -1,2 +1,21 @@
 class FizzBuzz {
+
+    fun convert(n: Int): String {
+        if (isFizz(n) && isBuzz(n)) {
+            return "FizzBuzz"
+        } else if (isFizz(n)) {
+            return "Fizz"
+        } else if (isBuzz(n)) {
+            return "Buzz"
+        }
+        return n.toString()
+    }
+
+    private fun isBuzz(n: Int): Boolean {
+        return n % 5 == 0
+    }
+
+    private fun isFizz(n: Int): Boolean {
+        return n % 3 == 0
+    }
 }
--- fizzbuzz-simple/src/test/kotlin/FizzBuzzTest.kt
+++ fizzbuzz-simple/src/test/kotlin/FizzBuzzTest.kt
@@ -1,6 +1,44 @@
+import de.cfranzen.kata.fizzbuzz.FizzBuzz
 import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 
 class FizzBuzzTest {
 
+    val sut = FizzBuzz()
+
+    @Test
+    fun returnsNumberItself() {
+        val inputs = listOf(1, 2, 4, 7, 8, 11)
+        for (n in inputs) {
+            val result = sut.convert(n)
+            assertThat(result).isEqualTo("$n")
+        }
+    }
+
+    @Test
+    fun returnsFizz() {
+        val inputs = listOf(3, 6, 9, 12)
+        for (n in inputs) {
+            val result = sut.convert(n)
+            assertThat(result).isEqualTo("Fizz")
+        }
+    }
+
+    @Test
+    fun returnsBuzz() {
+        val inputs = listOf(5, 10, 20, 25)
+        for (n in inputs) {
+            val result = sut.convert(n)
+            assertThat(result).isEqualTo("Buzz")
+        }
+    }
+
+    @Test
+    fun returnsFizzBuzz() {
+        val inputs = listOf(0, 15, 30, 45)
+        for (n in inputs) {
+            val result = sut.convert(n)
+            assertThat(result).isEqualTo("FizzBuzz")
+        }
+    }
 }
